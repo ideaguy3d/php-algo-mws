1e7dbc581929a382988c59db9b5dafbcfc63697b
Julius Alvarado <angularjs.uiux@gmail.com>
2018-04-14 18:56:34 -0700

    'case' SQL prac ^_^

--- sql2.sql
+++ sql2.sql
@@ -51,3 +51,62 @@ SELECT * FROM  movies
 WHERE genre = 'comedy' OR genre = 'romance'
 ORDER BY -imdb_rating;
 
+/* ----------------------
+   ---- commit point ----
+   ---------------------- */
+
+/* 'ORDER BY' prac */
+SELECT `name`, `year` FROM movies
+ORDER BY `name` ASC;
+
+SELECT `name`, `year`, imdb_rating FROM movies
+ORDER BY `year` DESC;
+
+/* 'LIMIT' prac */
+SELECT * FROM movies
+ORDER BY imdb_rating LIMIT 3;
+
+/* 'CASE' prac */
+SELECT `name`,
+	CASE
+  	WHEN imdb_rating > 8 THEN "Spetacular movie!"
+    WHEN imdb_rating > 6 THEN "Not that good."
+    ELSE "A pretty awful movie"
+  END AS 'zREVIEW'
+FROM movies;
+
+SELECT `name`,
+  CASE
+    WHEN `genre` = 'romance' THEN 'chill'
+    WHEN `genre` = 'comedy' THEN 'chill'
+    ELSE 'intense'
+  END AS 'Mood'
+FROM movies;
+
+SELECT
+	`amazon_order_id`,
+    `product_name`,
+	CASE
+    	WHEN `item_price` > 10 THEN 'Expensive'
+        WHEN `item_price` > 1 THEN 'Affordable'
+        ELSE 'unknown'
+    END AS 'economics'
+FROM `majide_test1`;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* end of this SQL file */
