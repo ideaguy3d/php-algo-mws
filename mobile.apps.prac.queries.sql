ddeb2119939878722ef8c2afc4f12fd37b5207cf
Julius Alvarado <angularjs.uiux@gmail.com>
2018-04-15 13:35:05 -0700

    aggregate prac

--- mobile.apps.prac.queries.sql
+++ mobile.apps.prac.queries.sql
@@ -36,3 +36,29 @@ SELECT category, SUM(downloads)
 FROM fake_apps
 GROUP BY category;
 
+/* ----------------------
+   ---- commit point ----
+   ---------------------- */
+
+SELECT category, price, AVG(downloads)
+FROM fake_apps
+GROUP BY category, price;
+
+SELECT category, price,
+  ROUND(AVG(downloads), 2) AS 'avg_downloads'
+FROM fake_apps
+GROUP BY 1, 2;
+
+SELECT price,
+  ROUND(AVG(downloads)) AS 'average_downloads'
+FROM fakes_apps
+GROUP BY 2
+HAVING COUNT(*) > 9;
+
+SELECT COUNT(*) AS 'total_rows_in_data_table'
+FROM fake_apps;
+
+SELECT category, COUNT(category) AS 'total number of apps in category'
+FROM fake_apps
+GROUP BY category
+HAVING COUNT(*) > 10;
