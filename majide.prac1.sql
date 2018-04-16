c2d9b0e0b6fe7eba9e073536f236ca843e86013a
Julius Alvarado <angularjs.uiux@gmail.com>
2018-04-15 18:33:11 -0700

    half way through the prac project

--- majide.prac1.sql
+++ majide.prac1.sql
@@ -3,7 +3,11 @@ SELECT `amazon_order_id`, SUM(`item_price`)
 FROM `majide_test1`
 GROUP BY `amazon_order_id`
 
-
+SELECT `amazon_order_id`, `order_status`
+  COUNT(*) AS 'total rows with this AOID',
+  SUM(item_price) AS 'sum for this AOID',
+FROM `majide_test1`
+GROUP BY `amazon_order_id`;
 
 
 
--- startup.trends.sql
+++ startup.trends.sql
@@ -2,3 +2,46 @@ SELECT COUNT(*) AS 'total companies',
 	SUM(valuation) AS 'total value',
   MAX(raised) AS 'most raised'
 FROM startups;
+
+/* simple query to get most raised
+  by seed stage start up */
+SELECT MAX(raised)
+FROM startups
+WHERE stage = 'seed';
+
+SELECT stage,
+  COUNT(*) AS 'total companies',
+  SUM(raised) AS 'total raised',
+  MAX(raised) AS 'max raised',
+FROM startups
+GROUP BY stage;
+
+SELECT stage,
+	COUNT(*) AS 'total companies',
+	SUM(valuation) AS 'total value',
+  MAX(raised) AS 'most raised'
+FROM startups
+GROUP BY stage
+HAVING stage NOT NULL;
+
+/* find the oldest startup */
+SELECT `name`,
+  MIN(founded) AS 'year founded',
+FROM startups
+GROUP BY `name`
+ORDER BY founded ASC
+LIMIT 3; /* get the 3 oldest */
+
+/* simpler way */
+SELECT *, MIN(founded)
+FROM startups;
+
+SELECT ROUND(AVG(valuation)) AS 'average of all startups'
+FROM startups;
+
+
+
+
+
+
+/* end of this SQL file */
