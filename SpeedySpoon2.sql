3c0a63d19a108124e4233251703836b9e1ac6f76
Julius Alvarado <angularjs.uiux@gmail.com>
2018-05-25 10:56:37 -0700

    more sql prac

--- SpeedySpoon2.sql
+++ SpeedySpoon2.sql
@@ -31,3 +31,47 @@ LIMIT 10;
 SELECT *
 FROM order_items
 LIMIT 10;
+
+
+/*
+  commit point
+*/
+
+
+/* total revenue per product name */
+SELECT
+	name,
+  ROUND(SUM(amount_paid), 2) AS total_revenue
+FROM order_items
+GROUP BY name
+ORDER BY 2 DESC;
+
+/* percent of revenue for each product */
+SELECT
+  `name` AS name_of_product,
+  ROUND(SUM(amount_paid) /
+  (SELECT SUM(amount_paid) FROM order_items) * 100, 2) AS pct
+FROM order_items
+GROUP BY 1,
+ORDER BY pct DESC;
+
+/* This query aggregates names to a category via a
+  case statement and group by */
+SELECT
+	case `name`
+  	when 'kale-smoothie'    then 'smoothie'
+    when 'banana-smoothie'  then 'smoothie'
+    when 'orange-juice'     then 'drink'
+    when 'soda'             then 'drink'
+    when 'blt'              then 'sandwich'
+    when 'grilled-cheese'   then 'sandwich'
+    when 'tikka-masala'     then 'dinner'
+    when 'chicken-parm'     then 'dinner'
+    else 'other'
+  end AS category,
+  ROUND(SUM(amount_paid) /
+        (SELECT SUM(amount_paid) FROM order_items) * 100, 2) as pct
+FROM order_items
+GROUP BY 1
+ORDER BY 2 DESC
+LIMIT 100;
