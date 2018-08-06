e7f50628f52d0ed3d59be5dd69199a9b90fd7aeb
Julius Alvarado <angularjs.uiux@gmail.com>
2018-08-05 20:38:05 -0700

    quick check

--- cp.php
+++ cp.php
@@ -11,13 +11,19 @@
 
 //-- in command prompt:
 // php cp.php "zip=95820&name=julius"
+//if (!isset($_SERVER["HTTP_HOST"])) {
+////    parse_str($argv[1], $_GET);
+////    $getZip = $_GET['zip'];
+////    $getName = $_GET['name'];
+////    echo "\n\n getZip = $getZip and getName = $getName \n\n";
+////}
 
-if (!isset($_SERVER["HTTP_HOST"])) {
-    parse_str($argv[1], $_GET);
-    $getZip = $_GET['zip'];
-    $getName = $_GET['name'];
-    echo "\n\n getZip = $getZip and getName = $getName \n\n";
-}
+// convert "V12345" to "sv12345"
+$sv = str_replace("v", "sv", strtolower("V12345"));
+
+echo "sv = $sv";
 
 
 
+
+// end of php file
