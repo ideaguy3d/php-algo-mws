88e4ae6dbf8c5542644a479bc35be7ff166163ea
Julius Alvarado <javascript.uiux@gmail.com>
2018-08-30 19:01:13 -0700

    practicing unit testing a bit

--- testing/src/Receipt.php
+++ testing/src/Receipt.php
@@ -4,11 +4,15 @@ namespace TDD;
 
 class Receipt
 {
-    public function total(array $items = []) {
-        return array_sum($items);
+    public function total(array $items, float $coupon): float {
+        $sum = array_sum($items);
+        if(!is_null($coupon)) {
+            return $sum - ($sum * $coupon);
+        }
+        return $sum;
     }
 
-    public function tax($amount, $tax) {
+    public function tax(int $amount, float $tax): float {
         return ($amount * $tax);
     }
 }
