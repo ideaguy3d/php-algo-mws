d02f39cc26081740ac39d1e8f4bc6c3e615ed49a
Julius Alvarado <javascript.uiux@gmail.com>
2018-08-30 19:06:28 -0700

    removed types in function declarations
    this was causing the tests to fail :frowning:

--- testing/src/Receipt.php
+++ testing/src/Receipt.php
@@ -4,7 +4,7 @@ namespace TDD;
 
 class Receipt
 {
-    public function total(array $items, float $coupon): float {
+    public function total(array $items, $coupon) {
         $sum = array_sum($items);
         if(!is_null($coupon)) {
             return $sum - ($sum * $coupon);
@@ -12,7 +12,7 @@ class Receipt
         return $sum;
     }
 
-    public function tax(int $amount, float $tax): float {
+    public function tax($amount,$tax) {
         return ($amount * $tax);
     }
 }
