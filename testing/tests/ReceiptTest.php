b081126b85fa0f129662405816b3547920dd8b55
Julius Alvarado <angularjs.uiux@gmail.com>
2018-09-03 01:46:08 -0700

    vendor\bin\phpunit --filter='testTotal@ints totaling 16'
    some advanced data provider usuage

--- testing/tests/ReceiptTest.php
+++ testing/tests/ReceiptTest.php
@@ -39,9 +39,9 @@ class ReceiptTest extends TestCase
 
     public function provideTotal() {
         return [
-            [[1,2,5,8], 16],
-            [[-1,2,5,8], 14],
-            [[1,2,8], 11],
+            "ints totaling 16" => [[1, 2, 5, 8], 16],
+            [[-1, 2, 5, 8], 14],
+            [[1, 2, 8], 11],
         ];
     }
 
