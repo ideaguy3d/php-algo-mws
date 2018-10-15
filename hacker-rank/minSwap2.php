6e0b5bbecb8d83916bc4035ba57a763a5be2cf6e
Julius Alvarado <angularjs.uiux@gmail.com>
2018-10-14 23:56:43 -0700

    current approach so far

--- hacker-rank/minSwap2.php
+++ hacker-rank/minSwap2.php
@@ -10,8 +10,30 @@
 
 $input1 = [1, 3, 5, 2, 4, 6, 8];
 
-function swap (array $a): int {
-    
+minimumSwap($input1);
+
+function minimumSwap (array $a): int {
+    // get reference to how the sorted array should look
+    $aCopy = $a;
+    sort($aCopy);
+
+    for ($i = 0, $sortedIdx = 0; $i < count($a); $i++) {
+        $rec = $a[$i];
+        $sortedRefRec = $aCopy[$sortedIdx];
+        // check if cur $rec is already smallest int in arr
+        if($rec === $sortedRefRec) {
+            $sortedIdx++;
+            continue;
+        }
+        // INNER LOOP :(
+        for($i2 = $i + 1; $i2 < (count($a) - $i); $i2++) {
+            $rec2 = $a[$i2];
+            if($rec2 < $rec) {
+                break;
+            }
+        }
+    }
+
     return 1;
 }
 
