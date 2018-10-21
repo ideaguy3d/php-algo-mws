54e44c2fd39ee67427b0d479bf0c0c5a50715ec5
Julius Alvarado <angularjs.uiux@gmail.com>
2018-10-20 22:33:45 -0700

    Update a1.php

--- a1.php
+++ a1.php
@@ -14,8 +14,14 @@ $firstLetter = $userinfo['username'][0];
 $a = 210;
 $b = 'a';
 
-echo $$b;
+$sale = 200;
+$sale = $sale- + 1; // 199
+$a = 20%-8; // 4
+
 echo "\n\n";
+echo $a;
+echo "\n\n";
+
 
 $letterUserName = strtoupper(str_split($userinfo['username'])[0]);
 $letterUserName2 = strtoupper($userinfo['username'][0]);
