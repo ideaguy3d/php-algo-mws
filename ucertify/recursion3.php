ee36f911a4f33f3669e5ffb7cb8ddcabb376c2ab
Julius Alvarado <javascript.uiux@gmail.com>
2019-04-10 13:42:52 -0700

    spent a lot of time studying algebraic sequences

--- ucertify/recursion3.php
+++ ucertify/recursion3.php
@@ -15,7 +15,7 @@ function fibonacci1(
         return $n;
     }
     $sum = array_sum($recurseArray);
-    echo " \n__>> {order: [$order], sum: $sum} recursion start __>> ";
+    echo " \n__>> {order: [$order], sum: $sum, n: $n} recursion start __>> ";
     $break = 'point';
     return fibonacci1($n - 2, '1st', $recurses, $recurseArray)
         + fibonacci1($n - 1, '2nd', $recurses, $recurseArray);
@@ -23,7 +23,12 @@ function fibonacci1(
 
 for($i = 0; $i < $iterations; $i++) {
     echo "\n\n\nincrementer = $i, fibonacci return:";
-    echo fibonacci1($i, 'origin', $recurses, $recurseArray) . " ~in $recurses recurses";
+    echo(
+        fibonacci1($i, 'origin', $recurses, $recurseArray)
+        . " ~in $recurses recurses"
+    );
+    
+    //-- reset:
     $recurses = 0;
     $recurseArray = [];
 }
